42fe3a359ab0096243cf397fde01af79313a5ea2
Stefan Hagen <stefan@hagen.link>
2023-08-01 23:10:09 +0200

    test: added more test fixtures and updted the adhoc 'tabla' tool
    - added more test fixtures for the adhoc table alignment and validator tool (tabla)
    - split the fixtures in abuse and use cases
    - updated the adhoc table alignment and validator tool (tabla)
    - script is documented and passes the usual quality controls:
      + mypy bin/adhoc-align-table-columns.py (for type verification)
      + ruff --ignore E501 bin/adhoc-align-table-columns.py (for general linting)
      + black -S -l120 bin/adhoc-align-table-columns.py (for 120 lines and single quotes preferred format)
      + vale --config bin/adhoc-align-table-columns.py (developer documentation spell check)
    - some typical errors with such tables have been provided as data in the abuse cases
      and 'tabla' has been extended to detect these. Developer documentation in the module
      has been updated to reflect this fact.
    - some refactoring and many fixes of the prototypical code
    - note: a commit will follow that amends the developer documentation (bin/README.md)
      including usage examples.
    
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- sarif-2.2/prose/edit/bin/adhoc-align-table-columns.py
+++ sarif-2.2/prose/edit/bin/adhoc-align-table-columns.py
@@ -32,6 +32,8 @@ Outgoing:
 |         |           |                    |
 ```
 
+Note: Some incorrect table representations raise attention through exceptions while others may slip through.
+
 This one off script expects a specific form of markdown not spreading a reference across two lines.
 The presence in the repository is merely for documentary purposes as it aided in the historic
 transformation of the SARIF v2.1.0 prose from a vendor proprietary format to markdown with
@@ -39,9 +41,11 @@ the goal to seed work on the SARIF v2.2 specification in markdown format.
 
 Note: The dashes in the filename indicate that this file is intended for execution only and not for import.
 """
+import datetime as dti
+import logging
 import pathlib
 import sys
-from typing import Union
+from typing import Union, no_type_check
 
 ENCODING = 'utf-8'
 DASH = '-'
@@ -59,120 +63,229 @@ FORMAT_LINE_LEFT = '|:'
 FORMAT_LINE_RIGHT = ':|'
 ALIGNMENT_CODES = ('l', 'c', 'r')  # left, center, right; unspecified -> left
 
-
-def table_in(text: str) -> bool:
-    """Detect if the text line contains a table line."""
-    return text.startswith(COLUMN_SEP)
-
-
-def section_ref(text: str) -> bool:
-    """Detect if the text references a section."""
-    return text.startswith('§') or text.startswith('Appendix ')
+# Logging on module level
+log = logging.getLogger()  # Module level logger is sufficient
+LOG_LEVEL = logging.INFO
+TS_FORMAT_LOG = '%Y-%m-%dT%H:%M:%S'
 
 
-def is_table_head_sep(text: str) -> bool:
-    """Detect a table head separating line per character removal and guard against empty or space only cells."""
-    allowed_chars = (ALIGN_MARK, COLUMN_SEP, HEAD_SEP, SPACE)
-    other = ''.join(set(text.split()))
-    for char in allowed_chars:
-        other = other.replace(char, '')
-    return bool(text.strip().replace(COLUMN_SEP, '')) and not other
+@no_type_check
+def formatTime_RFC3339(self, record, datefmt=None):
+    """HACK A DID ACK we could inject .astimezone() to localize ..."""
+    return dti.datetime.fromtimestamp(record.created, dti.timezone.utc).isoformat()
 
 
-def parse_table_col_widths(tokens: list[str]) -> list[int]:
-    """Parse table token lists for column widths."""
-    return [len(text) for text in tokens]
+@no_type_check
+def init_logger(name=None, level=None):
+    """Initialize module level logger"""
+    global log
+
+    log_format = {
+        'format': '%(asctime)s %(levelname)s [%(name)s]: %(message)s',
+        'datefmt': TS_FORMAT_LOG,
+        'level': LOG_LEVEL if level is None else level,
+    }
+    logging.Formatter.formatTime = formatTime_RFC3339
+    logging.basicConfig(**log_format)
+    log = logging.getLogger('tool' if name is None else name)
+    log.propagate = True
 
 
-def table_col_widths(col_widths: list[list[int]]) -> list[int]:
-    """Aggregate column widths per maximum for table wide column widths."""
-    table_spec = col_widths[0]
-    for widths in col_widths[1:]:
-        for slot, width in enumerate(widths):
-            table_spec[slot] = max(table_spec[slot], width)
-    return table_spec
+init_logger(name='tabla', level=None)
 
 
-def parse_table_head_sep(tokens: list[str]) -> list[str]:
-    """Parse table head separator token list for column count and their horizontal alignments."""
-    alignments = []
-    for text in tokens:
-        left = text.startswith(ALIGN_MARK)
-        right = text.endswith(ALIGN_MARK)
-        code = 'c' if left and right else ('l' if left else ('r' if right else 'l'))
-        alignments.append(code)
-    return alignments
+class Table:
+    """A GFM format markdown table."""
 
+    def __init__(self, resource: pathlib.Path, offset: int, table_lines: list[str]):
+        """Load from table lines into the internal data structures."""
+        self.resource = resource
+        self.offset: Union[int, None] = offset
+        self.lines: list[str] = [line for line in table_lines]
+        self.line_count = len(self.lines)
+        self.end_index = self.offset + self.line_count  # type: ignore
+        self.table_line_data: list[tuple[list[str], bool]] = [
+            Table.parse_table_line(line.rstrip(NL)) for line in self.lines
+        ]
+        self.alignments: list[str]
+        self.widths: list[list[int]] = []
+        self.spec: list[int]
 
-def parse_table_line(text: str) -> tuple[list[str], bool]:
-    """Parse a table line into an annotated token list assuming no pipe in cells.
+    def separator_line_count(self) -> int:
+        """Return the number of separator lines."""
+        return sum(1 if is_sep else 0 for _, is_sep in self.table_line_data)
 
-    The text '| a | b |' yields ([' a ', ' b '], False).
-    The text '|:--|--:|' yields ([':--', '--:'], True).
-    """
-    return [cell.strip() for cell in text.split(COLUMN_SEP)[1:-1]], is_table_head_sep(text)
+    def validate_spec(self) -> bool:
+        """Validate the table specification."""
+        sep_line_count = self.separator_line_count()
+        if sep_line_count == 1:
+            return True
+        diagnose = '(no separator line detected)' if not sep_line_count else '(ambiguous separator lines detected)'
+        message = (
+            f'expected 1 separator line but found {sep_line_count}'
+            f' in location({self.resource.name}:{self.end_index + 1}@table[{self.line_count}]) -> {diagnose}'
+        )
+        log.error(message)
+        return False
 
+    def derive_table_spec(self) -> list[int]:
+        """Derive alignments and column widths from data."""
+        column_count_guard = None
+        # Derive the specification of table wide column widths and alignments
+        for where, entry in enumerate(self.table_line_data):
+            detected_col_count = len(entry[0])
+            if column_count_guard is None:
+                column_count_guard = detected_col_count
+            elif column_count_guard != detected_col_count:
+                message = (
+                    f'expected cols({column_count_guard}) but found cols({detected_col_count})'
+                    f' in location({self.resource.name}:{self.offset + where + 1}@table[{where}]) ->'
+                    f' ({self.lines[where].rstrip(NL)})'
+                )
+                log.error(message)
+                return []
+            if not entry[1]:
+                self.widths.append(Table.parse_table_col_widths(entry[0]))
+            else:
+                self.widths.append([len(':-:') for _ in entry[0]])  # minimum width of three for separator cells
+                self.alignments = Table.parse_table_head_sep(entry[0])
+        self.spec = Table.table_col_widths(self.widths)
+        return self.spec
 
-def left(text: str, width: int, fill: str = SPACE) -> str:
-    """Left align the content text left and fill up to the width with fill (default: spaces)."""
-    return text.ljust(width, fill)
+    def render_text_row(self, header_tokens: list[str]) -> str:
+        """Render a table header (column labels) or data line according to column alignments and widths.
 
+        The row width segregation is(m: marker single space, c: aligned content):
+            |mcm|mcm|...m|
 
-def center(text: str, width: int, fill: str = SPACE) -> str:
-    """Center the content text left and fill up to the width with fill (default: spaces)."""
-    return text.center(width, fill)
+        The content is either left aligned (code 'l'), right aligned (code 'r'), or centered ('c').
 
+        The result receives a trailing new line to simplify the patch upstream.
+        """
+        data = []
+        for text, code, width in zip(header_tokens, self.alignments, self.spec):
+            if code == 'l':
+                cell = Table.left(text, width)
+            elif code == 'c':
+                cell = Table.center(text, width)
+            else:
+                cell = Table.right(text, width)
+            # Effective width has to be amended for the width of the two alignment markers
+            data.append(SPACE + cell + SPACE)
+        return COLUMN_SEP + COLUMN_SEP.join(data) + COLUMN_SEP + NL
 
-def right(text: str, width: int, fill: str = SPACE) -> str:
-    """Right align the content text left and fill up to the width with fill (default: spaces)."""
-    return text.rjust(width, fill)
+    def render_separator_row(self, ignored_tokens: list[str]) -> str:
+        """Derive the table separator line according from column alignments and widths.
 
+        The row width segregation is(m and n: marker single spaces (left and right), f: fill characters '-'):
+            |mfn|mfn|...n|
 
-def render_text_row(header_tokens: list[str], alignments: list[str], widths: list[int]) -> str:
-    """Render a table header (column labels) or data line according to column alignments and widths.
+        The left marker is either ':' when column is left aligned (code 'l') or centered ('c').
+        The right marker is either ':' when column is centered ('c') or right aligned (code 'r').
 
-    The row width segregation is(m: marker single space, c: aligned content):
-        |mcm|mcm|...m|
+        The result receives a trailing new line to simplify the patch upstream.
+        """
+        data = []
+        for _, code, width in zip(ignored_tokens, self.alignments, self.spec):
+            inner_text = HEAD_SEP * width
+            if code == 'l':
+                left, right = ALIGN_MARK, HEAD_SEP
+            elif code == 'c':
+                left, right = ALIGN_MARK, ALIGN_MARK
+            else:
+                left, right = HEAD_SEP, ALIGN_MARK
+            data.append(left + inner_text + right)
+        return COLUMN_SEP + COLUMN_SEP.join(data) + COLUMN_SEP + NL
 
-    The content is either left aligned (code 'l'), right aligned (code 'r'), or centered ('c').
+    def render(self) -> None:
+        """Render the table in-place into the lines."""
+        for where, (data, is_sep) in enumerate(self.table_line_data):
+            if not is_sep:
+                line = self.render_text_row(data)
+            else:
+                line = self.render_separator_row(data)
+            self.lines[where] = line
 
-    The result receives a trailing new line to simplify the patch upstream.
-    """
-    data = []
-    for text, code, width in zip(header_tokens, alignments, widths):
-        if code == 'l':
-            cell = left(text, width)
-        elif code == 'c':
-            cell = center(text, width)
-        else:
-            cell = right(text, width)
-        # Effective width has to be amended for the width of the two alignment markers
-        data.append(SPACE + cell + SPACE)
-    return COLUMN_SEP + COLUMN_SEP.join(data) + COLUMN_SEP + NL
+    @staticmethod
+    def table_in(text: str) -> bool:
+        """Detect if the text line contains a table line."""
+        return text.startswith(COLUMN_SEP)
 
+    @staticmethod
+    def is_table_head_sep(text: str) -> bool:
+        """Detect a table head separating line per character removal and guard against empty or space only cells."""
+        if HEAD_SEP not in text or not text.strip().replace(COLUMN_SEP, ''):
+            return False
+        allowed_chars = (ALIGN_MARK, HEAD_SEP, COLUMN_SEP, SPACE)
+        other = ''.join(set(text.split()))
+        for char in allowed_chars:
+            other = other.replace(char, '')
+        return not other
 
-def render_separator_row(ignored_tokens: list[str], alignments: list[str], widths: list[int]) -> str:
-    """Derive the table separator line according from column alignments and widths.
+    @staticmethod
+    def parse_table_col_widths(tokens: list[str]) -> list[int]:
+        """Parse table token lists for column widths."""
+        return [len(text) for text in tokens]
 
-    The row width segregation is(m and n: marker single spaces (left and right), f: fill characters '-'):
-        |mfn|mfn|...n|
+    @staticmethod
+    def table_col_widths(col_widths: list[list[int]]) -> list[int]:
+        """Aggregate column widths per maximum for table wide column widths."""
+        table_spec = col_widths[0]
+        for widths in col_widths[1:]:
+            for slot, width in enumerate(widths):
+                table_spec[slot] = max(table_spec[slot], width)
+        return table_spec
 
-    The left marker is either ':' when column is left aligned (code 'l') or centered ('c').
-    The right marker is either ':' when column is centered ('c') or right aligned (code 'r').
+    @staticmethod
+    def parse_table_head_sep(tokens: list[str]) -> list[str]:
+        """Parse table head separator token list for column count and their horizontal alignments."""
+        alignments = []
+        for text in tokens:
+            left = text.startswith(ALIGN_MARK)
+            right = text.endswith(ALIGN_MARK)
+            code = 'c' if left and right else ('l' if left else ('r' if right else 'l'))
+            alignments.append(code)
+        return alignments
 
-    The result receives a trailing new line to simplify the patch upstream.
-    """
-    data = []
-    for _, code, width in zip(ignored_tokens, alignments, widths):
-        inner_text = HEAD_SEP * width
-        if code == 'l':
-            left, right = ALIGN_MARK, HEAD_SEP
-        elif code == 'c':
-            left, right = ALIGN_MARK, ALIGN_MARK
-        else:
-            left, right = HEAD_SEP, ALIGN_MARK
-        data.append(left + inner_text + right)
-    return COLUMN_SEP + COLUMN_SEP.join(data) + COLUMN_SEP + NL
+    @staticmethod
+    def parse_table_line(text: str) -> tuple[list[str], bool]:
+        """Parse a table line into an annotated token list assuming no pipe in cells.
+
+        The text '| a | b |' yields ([' a ', ' b '], False).
+        The text '|:--|--:|' yields ([':--', '--:'], True).
+        """
+        return [cell.strip() for cell in text.split(COLUMN_SEP)[1:-1]], Table.is_table_head_sep(text)
+
+    @staticmethod
+    def left(text: str, width: int, fill: str = SPACE) -> str:
+        """Left align the content text left and fill up to the width with fill (default: spaces)."""
+        return text.ljust(width, fill)
+
+    @staticmethod
+    def center(text: str, width: int, fill: str = SPACE) -> str:
+        """Center the content text left and fill up to the width with fill (default: spaces)."""
+        return text.center(width, fill)
+
+    @staticmethod
+    def right(text: str, width: int, fill: str = SPACE) -> str:
+        """Right align the content text left and fill up to the width with fill (default: spaces)."""
+        return text.rjust(width, fill)
+
+    def validate_post_table_line(self, text: str, last_line_is_table: bool) -> bool:
+        """Blank or table was already last line of file is valid."""
+        blank = not bool(text.rstrip(NL))
+        has_col_sep = bool(text.lstrip().startswith(COLUMN_SEP))
+        if last_line_is_table or blank and not has_col_sep:
+            return True
+        # We enforce blank line after tables
+        diagnose = f'column separator ({COLUMN_SEP})' if COLUMN_SEP in text else 'non-blank line'
+        message = (
+            f'expected either table, blank line, or end of file but found {diagnose}'
+            f' in location({self.resource.name}:{self.end_index + 1}@table[{self.line_count + 1}]) ->'
+            f' ({text.rstrip(NL)})'
+        )
+        log.error(message)
+        return False
 
 
 def main(argv: list[str]) -> int:
@@ -183,45 +296,51 @@ def main(argv: list[str]) -> int:
         with open(resource, 'rt', encoding=ENCODING) as handle:
             lines = handle.readlines()
         table_lines = []
-        table_line_data: list[tuple[list[str], bool]] = []
         in_table = False
         first_table_line: Union[int, None] = None
+        last_line_is_table = False
         for slot, line in enumerate(lines):
-            if table_in(line):
+            if Table.table_in(line):
                 if first_table_line is None:
                     first_table_line = slot
+                    last_line_is_table = False
                     in_table = True
                 table_lines.append(line)
-                table_line_data.append(parse_table_line(line.rstrip(NL)))
-                if slot < len(lines) - 1:
-                    # Base case insurance
-                    continue
-            if in_table:
-                # Process the table and inject the patch the upstream lines
-                col_widths = []
-                # Derive the specification of table wide column widths and alignments
-                for where, entry in enumerate(table_line_data):
-                    if not entry[1]:
-                        col_widths.append(parse_table_col_widths(entry[0]))
-                    else:
-                        col_widths.append([len(':-:') for _ in entry[0]])  # minimum width of three for separator cells
-                        alignments = parse_table_head_sep(entry[0])
-                table_spec = table_col_widths(col_widths)
-                # Render the table rows into the table lines
-                for where, (data, is_sep) in enumerate(table_line_data):
-                    if not is_sep:
-                        line = render_text_row(data, alignments, table_spec)
-                    else:
-                        line = render_separator_row(data, alignments, table_spec)
-                    table_lines[where] = line
+                continue
+            if slot == len(lines):
+                # Base case insurance
+                log.warning(f'File {resource.name} ends with table line')
+                last_line_is_table = in_table
 
-                lines[first_table_line : first_table_line + len(table_lines)] = table_lines  # type: ignore
+            if in_table:
+                # In file loop processing
+                table = Table(resource, first_table_line, table_lines)  # type: ignore
+                if not table.validate_post_table_line(line, last_line_is_table=last_line_is_table):
+                    return 1
+                if not table.validate_spec():
+                    return 1
+                if not table.derive_table_spec():
+                    return 1
+                table.render()
+                lines[table.offset : table.offset + len(table.lines)] = table.lines  # type: ignore
+
+                # Reset processing structures for further tables
                 table_lines = []
                 in_table = False
 
+        # Post file loop processing - in case the table is at the end of the file (resource)
+        if table_lines:
+            table = Table(resource, first_table_line, table_lines)  # type: ignore
+            if not table.validate_spec():
+                return 1
+            if not table.derive_table_spec():
+                return 1
+            table.render()
+            lines[table.offset : table.offset + len(table.lines)] = table.lines  # type: ignore
+
         with open(BUILD_AT / resource.name, 'wt', encoding=ENCODING) as handle:
             handle.write(''.join(lines))
-        print(f'document with aligned tables written to {BUILD_AT / resource.name}', file=sys.stderr)
+        log.info(f'document with aligned tables written to {BUILD_AT / resource.name}')
 
     return 0
 
---
+++ sarif-2.2/prose/edit/test/fixtures/adhoc/use/misaligned-two-gfm-tables.md
@@ -0,0 +1,21 @@
+|    nothing | something     |     a long column head |
+|:---|----:|:---------:|
+| 1  | 42  |       999 |
+| left|right |centered|
+||||
+
+Table: Caption for table.
+
+# Some Section
+
+A paragraph.
+
+|    nothing | something     |     a long column head |
+|:---|----:|:---------:|
+| 1  | 42  |       999 |
+| left|right |centered|
+||||
+
+Table: Another table caption.
+
+Some final paragraph.
