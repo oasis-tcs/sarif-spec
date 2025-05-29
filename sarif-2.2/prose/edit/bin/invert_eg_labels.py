d0a160da4fd8726ed5186cad10be1a6d0468c20f
Stefan Hagen <stefan@hagen.link>
2025-05-29 20:11:35 +0200

    Enhanced the inverter tools to reduce the chores
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- sarif-2.2/prose/edit/bin/invert_eg_labels.py
+++ sarif-2.2/prose/edit/bin/invert_eg_labels.py
@@ -1,20 +1,48 @@
-#! /usr/bin/env python 
+#! /usr/bin/env python
 """Bread and butter inversion of global example number to example sug mapping."""
 import json
 import pathlib
 
 ETC_PATH = pathlib.Path('etc')
-IN_PATH = ETC_PATH / 'example-global-to-local.json'
-OUT_PATH = ETC_PATH / 'example-local-to-global.json'
+G2L_PATH = ETC_PATH / 'example-global-to-local.json'
+L2G_PATH = ETC_PATH / 'example-local-to-global.json'
+
+ENCODING = 'utf-8'
+ENC_ERRS = 'ignore'
+NL = '\n'
 
 if not ETC_PATH.is_dir():
-    raise RuntimeError('Please execute me inside csaf_2.1/prose/edit/ because I am a simple tool')
+    raise RuntimeError('Please execute me inside sarif_2.2/prose/edit/ because I am a simple tool')
 
-with open(IN_PATH, 'rt', encoding='utf-8') as handle:
+with open(G2L_PATH, 'rt', encoding=ENCODING, errors=ENC_ERRS) as handle:
     data = json.load(handle)
 
-inverted = {v: k for k, v in data.items()}
-ordered = {k: inverted[k] for k in sorted(inverted)}
+marker_value = "4321"
+marker_seen = False
+auto_serial = 1
+cleansed = {}
+for k, v in data.items():
+    if not marker_seen:
+        if k == marker_value:
+            marker_seen = True
+            cleansed[marker_value] = v
+            continue
+    cleansed[str(auto_serial)] = v
+    auto_serial += 1
 
-with open(OUT_PATH, 'wt', encoding='utf-8') as handle:
+with open(G2L_PATH, 'wt', encoding=ENCODING, errors=ENC_ERRS) as handle:
+    json.dump(cleansed, handle, indent=2)
+    handle.write(NL)  # For POSIX compliance
+
+inverted = {v: k for k, v in cleansed.items()}
+ordered = {
+    'Please do not edit manually!': (
+        "Instead, call 'make invert-examples' inside the clone-root/sarif_2.2/prose/edit folder."
+    )
+}
+for k in sorted(inverted):
+    ordered[k] = inverted[k]
+
+with open(L2G_PATH, 'wt', encoding=ENCODING, errors=ENC_ERRS) as handle:
     json.dump(ordered, handle, indent=2)
+    handle.write(NL)  # For POSIX compliance
--- sarif-2.2/prose/edit/bin/invert_sec_labels.py
+++ sarif-2.2/prose/edit/bin/invert_sec_labels.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python 
+#! /usr/bin/env python
 """Bread and butter inversion of section numbers to label mapping."""
 import json
 import pathlib
@@ -7,10 +7,14 @@ ETC_PATH = pathlib.Path('etc')
 IN_PATH = ETC_PATH / 'section-display-to-label.json'
 OUT_PATH = ETC_PATH / 'section-label-to-display.json'
 
-if not ETC_PATH.is_dir():
-    raise RuntimeError('Please execute me inside csaf_2.1/prose/edit/ because I am a simple tool')
+ENCODING = 'utf-8'
+ENC_ERRS = 'ignore'
+NL = '\n'
 
-with open(IN_PATH, 'rt', encoding='utf-8') as handle:
+if not ETC_PATH.is_dir():
+    raise RuntimeError('Please execute me inside sarif_2.2/prose/edit/ because I am a simple tool')
+
+with open(IN_PATH, 'rt', encoding=ENCODING, errors=ENC_ERRS) as handle:
     data = json.load(handle)
 
 unique = {v for v in data.values()}
@@ -26,7 +30,14 @@ if sorted(unique) != sorted(data.values()):
     raise RuntimeError('Please ensure the section labels are unique')
 
 inverted = {v: k for k, v in data.items()}
-ordered = {k: inverted[k] for k in sorted(inverted)}
+ordered = {
+    'Please do not edit manually!': (
+        "Instead, call 'make invert-sections' inside the clone-root/sarif_2.2/prose/edit folder."
+    )
+}
+for k in sorted(inverted):
+    ordered[k] = inverted[k]
 
-with open(OUT_PATH, 'wt', encoding='utf-8') as handle:
+with open(OUT_PATH, 'wt', encoding=ENCODING, errors=ENC_ERRS) as handle:
     json.dump(ordered, handle, indent=2)
+    handle.write(NL)  # For POSIX compliance
